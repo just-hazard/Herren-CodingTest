933bb97fad39c447a4bd3f1f7251dde74bfbe0b1
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-08-11 03:36:14 +0900

    주석 제거

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         // Sort
-//        Integer[] array = SortAlgorithm.randomArrays();
-//        SortAlgorithm.sort(array);
-//        SortAlgorithm.streamSort(array);
-//        String result = Arrays.toString(SortAlgorithm.quickSort(array, 0, array.length - 1));
-//        System.out.println(result);
+        Integer[] array = SortAlgorithm.randomArrays();
+        SortAlgorithm.sort(array);
+        SortAlgorithm.streamSort(array);
+        String result = Arrays.toString(SortAlgorithm.quickSort(array, 0, array.length - 1));
+        System.out.println(result);
 
         // Consonant
         String str = "우리나라 만세";
